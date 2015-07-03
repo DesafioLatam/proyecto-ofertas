cf3f3701594c3d818c95879e16cb62c5a888aceb
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-07-03 16:53:30 -0500

    adding bootstrap as sass

--- test/controllers/bids_controller_test.rb
+++ test/controllers/bids_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class BidsControllerTest < ActionController::TestCase
-  test "should get create" do
+  include Devise::TestHelpers
+
+  test "guest shouldnt create bids" do
+    user = users(:guest)
+    sign_in user
+
+    post :create
+    assert_redirected_to landing_page_path
+  end
+
+  test "user should create bids" do
+    post :create
+    assert_redirected_to products_path
+  end
+
+  test "seller shouldnt create bids" do
     post :create
     assert_response :success
   end
-
 end
--- test/models/bid_test.rb
+++ test/models/bid_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class BidTest < ActiveSupport::TestCase
+  def setup
+    @bid = bids(:one)
+  end
+
   # test "the truth" do
   #   assert true
   # end
+  test 'should have an user' do
+    @bid.user = nil
+    assert_not bid.valid?
+  end
+
+  test 'should have a product' do
+    @bid.product = nil
+    assert_not bid.valid?
+  end
+
+
 end
