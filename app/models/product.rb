5e52668c8844b8a42c98adebf80aa3f2dad35d11
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-07-02 20:20:17 -0500

    adding role to users, adding cancancan and the abilites

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :bids 
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :bids, only: [:create]
   end
 
+  root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
