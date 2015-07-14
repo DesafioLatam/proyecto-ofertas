54e717a27a6080f6f9bf0e6d5ad62aac387351ee
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-07-13 20:20:25 -0500

    users can bids twice consecutive on the same product

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,4 +4,17 @@ class Bid < ActiveRecord::Base
   validates :user_id, presence: true
   validates :product_id, presence: true
   #validates :amount, :numericality => { :greater_than => 0}
+
+  validate :consecutive, on: :create
+
+  def consecutive
+    puts 
+    return if self.product.nil?
+    return if self.product.bids.empty?
+
+    if self.user_id == self.product.bids.last.user_id
+      errors.add(:consecutive, "cannot bid twice in consecutive") 
+    end
+  end
+
 end
--- test/models/bid_test.rb
+++ test/models/bid_test.rb
@@ -19,4 +19,19 @@ class BidTest < ActiveSupport::TestCase
   end
 
 
+  test "same user cannot bid two consecutive times" do
+    @bid = Bid.create(user: users(:user), product: products(:one))
+    @bid2 = Bid.new(user: users(:user), product: products(:one))
+    assert !@bid2.valid?, "same user cannot bid two consecutive times"
+  end
+
+  test "same user can bid if another user bid after him" do
+    @bid = Bid.create(user: users(:user), product: products(:one))
+    @bid2 = Bid.create(user: users(:user2), product: products(:one))
+    @bid3 = Bid.new(user: users(:user), product: products(:one))
+    assert @bid3.valid?, "user can bid if another user bid after him but: #{@bid.errors.messages} #{@bid2.errors.messages} #{@bid3.errors.messages}"
+  end
+
+
+
 end
